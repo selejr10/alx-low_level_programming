d8106db11cc0f4972cad970a87917231b9f6504b
selejr10 <selebest10@gmail.com>
2023-08-14 07:31:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Put your comment here
+ *
+ * Return: 0 (succeeded)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	Return (0);
+}
