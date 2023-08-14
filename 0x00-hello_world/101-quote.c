12385b02270ce524dd9faec800b3af6b53644428
selejr10 <selebest10@gmail.com>
2023-08-14 08:43:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - A program that prints exactly and that piece of art is useful
- *
+ * main - A c program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-    return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
