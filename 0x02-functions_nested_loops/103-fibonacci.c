77d6a038427c285d3d441d0d773087fd891ca6b7
selejr10 <selebest10@gmail.com>
2023-08-16 11:16:09 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,10 +12,10 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2, sum = k;
 
-	while (k +j < 4000000)
+	while (k + j < 4000000)
 	{
 	k += j;
-	if (k % 2 ==0)
+	if (k % 2 == 0)
 	sum += k;
 	j = k - j;
 	++i;
