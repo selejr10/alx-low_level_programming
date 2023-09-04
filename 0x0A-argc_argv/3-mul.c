27153b185ec5960d42282023210d17ec0648b4be
selejr10 <selebest10@gmail.com>
2023-09-04 13:34:46 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,34 +12,34 @@ int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
-	i = 0;
+	p = 0;
 	d = 0;
 	n = 0;
 	len = 0;
-	f = 0;
+	r = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
-	while (i < len && f == 0)
+	while (p < len && r == 0)
 	{
-		if (s[i] == '-')
-			d++;
+		if (s[p] == '-')
+			++d;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[p] >= '0' && s[p] <= '9')
 		{
-			digit = s[i] - '0';
+			digit = s[p] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[p + 1] < '0' || s[p + 1] > '9')
 				break;
-			f = 0;
+			r = 0;
 		}
-		i++;
+		p++;
 	}
-	if (f == 0)
+	if (r == 0)
 
 		return (0);
 	return (n);
